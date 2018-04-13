51ad81fd6b247d8ad1cb3a5f5ab7dc2c526a8155
shimingyue <shimingyue@DESKTOP-2AHK92T>
2018-04-13 16:18:04 +0800

    123

---
+++ app/src/main/java/com/yue/tilemap/ui/gaode/TgdGoogleActivity.java
@@ -0,0 +1,18 @@
+package com.yue.tilemap.ui.gaode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yue.tilemap.R;
+
+/**
+ * google地图贴图贴在高德地图上
+ */
+public class TgdGoogleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tgd_google);
+    }
+}
