2b82b7922e70771b88ae42e82a5f5430ba11625b
shimingyue <shimingyue@DESKTOP-2AHK92T>
2018-04-17 13:28:09 +0800

    高德贴图

--- app/src/main/java/com/yue/tilemap/ui/gaode/TgdGoogleOfflineActivity.java
+++ app/src/main/java/com/yue/tilemap/ui/gaode/TgdGoogleOfflineActivity.java
@@ -3,21 +3,37 @@ package com.yue.tilemap.ui.gaode;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.model.TileOverlayOptions;
+import com.amap.api.maps.model.UrlTileProvider;
 import com.yue.tilemap.R;
 import com.yue.tilemap.databinding.ActivityTgdGoogleOfflineBinding;
 
+import java.net.URL;
+
 /**
  * 离线加载地图
+ * 注意缩放级别的控制
  */
 public class TgdGoogleOfflineActivity extends AppCompatActivity {
 
     private ActivityTgdGoogleOfflineBinding mBinding;
 
+    private AMap aMap;
+    //lyrs 表示图层
+    final String url = "http://a.tile.openstreetmap.org/%d/%d/%d.png";
+    String googleUrl01 = "http://mt0.google.cn/vt/lyrs=m&hl=zh-CN&gl=cn";
+    String googleUrl02 = "http://mt1.google.cn/vt/lyrs=m&hl=zh-CN&gl=cn";
+    String googleUrl03 = "http://mt2.google.cn/vt/lyrs=r&hl=zh-CN&gl=cn";
+    String googleUrl04 = "http://mt3.google.cn/vt/lyrs=m&hl=zh-CN&gl=cn";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tgd_google_offline);
+        initGdMap(savedInstanceState);
     }
 
     /**
@@ -25,14 +41,19 @@ public class TgdGoogleOfflineActivity extends AppCompatActivity {
      *
      * @param savedInstanceState
      */
-    private void initMap(Bundle savedInstanceState) {
-
+    private void initGdMap(Bundle savedInstanceState) {
+        mBinding.mapTgdgoogleOffline.onCreate(savedInstanceState);
+        if (aMap == null) {
+            aMap = mBinding.mapTgdgoogleOffline.getMap();
+            initTile(savedInstanceState);
+        }
     }
 
     /**
      * 初始化瓦片
      */
-    private void initTile() {
+    private void initTile(Bundle savedInstanceState) {
+        showOnlineTile();
 
     }
 
@@ -41,7 +62,26 @@ public class TgdGoogleOfflineActivity extends AppCompatActivity {
      * 在线瓦片 显示
      */
     private void showOnlineTile() {
+        aMap.addTileOverlay(new TileOverlayOptions().tileProvider(new UrlTileProvider(256, 256) {
 
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {
+                try {
+                    Log.i("TileOverlayActivity", "url:" + String.format(googleUrl03, zoom, x, y));
+//					return new URL(String.format(url, zoom, x, y));
+                    return new URL(googleUrl03 + "&x=" + x + "&y=" +
+                            y + "&z=" + zoom);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        })
+                .diskCacheEnabled(true)
+                .diskCacheDir("/storage/emulated/0/amap/cache")
+                .diskCacheSize(100000)
+                .memoryCacheEnabled(true)
+                .memCacheSize(100000));
     }
 
 
@@ -51,4 +91,34 @@ public class TgdGoogleOfflineActivity extends AppCompatActivity {
     private void showOffLineTile() {
 
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mBinding.mapTgdgoogleOffline.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mBinding.mapTgdgoogleOffline.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mBinding.mapTgdgoogleOffline.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mBinding.mapTgdgoogleOffline.onSaveInstanceState(outState);
+    }
+
 }
